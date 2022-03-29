7d4d0aea52f0533dceb737b1c320ed1ad10b25b5
JarcauCristian <stefan.jarcau@student.unitbv.ro>
2022-03-29 21:37:32 +0300

    TEr

--- src/main/java/de/jpp/factory/IOFactory.java
+++ src/main/java/de/jpp/factory/IOFactory.java
@@ -14,6 +14,7 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.jdom2.*;
 
 public class IOFactory {
 
