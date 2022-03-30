92669138beb8977839bea197a539311a3c3b8a7f
JarcauCristian <stefan.jarcau@student.unitbv.ro>
2022-03-30 14:36:14 +0300

    1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,13 +3,9 @@ module com.example.graphen {
     requires javafx.fxml;
     requires java.desktop;
     requires gxl;
-<<<<<<< HEAD
     requires java.security.jgss;
     requires org.jdom2;
     requires jdom2;
-=======
-    requires org.jdom2;
->>>>>>> master
 
 
     opens com.example.graphen to javafx.fxml;
